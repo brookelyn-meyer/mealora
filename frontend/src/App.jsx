441ff6b9037375c8b89e364822c6a4d3162d67bd
Brookelyn Meyer <bmeyer4s@semo.edu>
2026-07-25 15:50:52 -0500

    Updating the react app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,30 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [groceries, setGroceries] = useState([]); // gives react a place to remember data
 
   useEffect(() => {
-    console.log("The page loaded!");
+    fetch("http://127.0.0.1:8000/groceries")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setGroceries(data);
+      });
   }, []);
 
   return (
     <>
       <h1>🍎 MealOra</h1>
-      <p>Loading groceries...</p>
+
+      {groceries.map((grocery) => ( // .map() is the react equivalent of for grocery in groceries (python loop!)
+        <div key={grocery[0]}>
+          <h3>{grocery[1]}</h3>
+          <p>{grocery[2]}</p>
+          <p>{grocery[3]}</p>
+          <p>{grocery[4]}</p>
+          <hr />
+        </div>
+      ))}
     </>
   );
 }
