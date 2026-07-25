b566ab5604c39a2775b11d8f0667de3dfbcfc816
Brookelyn Meyer <bmeyer4s@semo.edu>
2026-07-25 16:35:31 -0500

    Add react grocery form and POST endpoint

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,41 @@ import "./App.css";
 
 function App() {
   const [groceries, setGroceries] = useState([]);
+  const [name, setName] = useState("");
+  const [quantity, setQuantity] = useState("");
+  const [location, setLocation] = useState("");
+  const [expirationDate, setExpirationDate] = useState("");
+
+async function addGrocery(event) {
+  event.preventDefault();
+
+  const response = await fetch("http://127.0.0.1:8000/groceries", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      name: name,
+      quantity: quantity,
+      location: location,
+      expiration_date: expirationDate || null,
+    }),
+  });
+
+  const newGrocery = await response.json();
+
+  setGroceries([...groceries, newGrocery]);
+
+  setName("");
+  setQuantity("");
+  setLocation("");
+  setExpirationDate("");
+}
+
+
+
+
+
 
   useEffect(() => {
   fetch("http://127.0.0.1:8000/groceries")
@@ -28,6 +63,39 @@ function App() {
         <h1>🍎 MealOra</h1>
         <p>Keep track of what you have before it expires.</p>
       </header>
+
+    <form onSubmit={addGrocery}>
+  <input
+    type="text"
+    placeholder="Name"
+    value={name}
+    onChange={(event) => setName(event.target.value)}
+  />
+
+  <input
+    type="text"
+    placeholder="Quantity"
+    value={quantity}
+    onChange={(event) => setQuantity(event.target.value)}
+  />
+
+  <input
+    type="text"
+    placeholder="Location"
+    value={location}
+    onChange={(event) => setLocation(event.target.value)}
+  />
+
+  <input
+    type="date"
+    value={expirationDate}
+    onChange={(event) => setExpirationDate(event.target.value)}
+  />
+
+  <button type="submit">
+    Add Grocery
+  </button>
+</form>
 
       <section className="grocery-list">
         {groceries.length === 0 ? (
